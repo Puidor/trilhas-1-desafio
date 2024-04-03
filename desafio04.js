53c27c35f2cf967a51e1ce30f1a303e990cdf2d1
Puidor <c.adriano.silva@hotmail.com>
2024-04-03 10:39:12 -0300

    add questão 4

--- desafio04.js
+++ desafio04.js
@@ -1,3 +1,27 @@
 // 4. Em uma pequena vila, o fazendeiro precisa separar os números primos para determinar quantos novos campos plantar. Ele pede sua ajuda para identificar esses números criando um algoritmos. Os números são: 23, 16, 11, 8, 19, 14, 5, 21.
 
+// Lista de números
 let numeros = [23, 16, 11, 8, 19, 14, 5, 21];
+
+// Função para verificar se é primo
+function verificarPrimo(numero) {
+  if (numero <= 1) {
+    return false;
+  }
+  for (let i = 2; i < numero; i++) {
+    if (numero % i == 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+let listaDosNumerosPrimos = [];
+
+for (let i = 0; i < numeros.length; i++) {
+  if (verificarPrimo(numeros[i])) {
+    listaDosNumerosPrimos.push(numeros[i]);
+  }
+}
+
+alert(listaDosNumerosPrimos);
