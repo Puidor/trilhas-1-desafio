27b8fad1aae295cfc2f9980d126858577716c2f0
Puidor <c.adriano.silva@hotmail.com>
2024-04-03 11:10:55 -0300

    add questao 7

---
+++ desafio07.js
@@ -0,0 +1,9 @@
+// 7.Em uma loja de conveniência, um cliente comprou três itens e precisa calcular o total da compra. Os preços dos itens são R$ 2,50, R$ 3,75 e R$ 1,99. Crie um algoritmo que retorne o valor total.
+
+let precoItem1 = 2.5;
+let precoItem2 = 3.75;
+let precoItem3 = 1.99;
+
+let totalCompra = precoItem1 + precoItem2 + precoItem3;
+
+alert("O total da compra é: R$" + totalCompra.toFixed(2));
